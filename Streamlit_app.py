1c3c724c1fa52fa7dd2f87216b8360fa85092316
javaidiqbal92 <javaidiqbalcourse@gmail.com>
2025-10-20 20:12:00 +0500

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -61,6 +61,6 @@ if st.button("Predict Churn"):
 
     # Display results
     if pred == 1:
-        st.error(f"Prediction: CHURN \n (Probability: {prob_churn:.4f})" if prob_churn is not None else "Prediction: **CHURN**")
+        st.error(f"Prediction: Churn \n (Probability: {prob_churn:.4f})" if prob_churn is not None else "Prediction: Churn")
     else:
-        st.success(f"Prediction: STAY\n (Probability: {1 - prob_churn:.4f})" if prob_churn is not None else "Prediction: **STAY**")
+        st.success(f"Prediction: Stay\n (Probability: {1 - prob_churn:.4f})" if prob_churn is not None else "Prediction: Stay")
